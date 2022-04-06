ee17c696b8acff7c2a2686e302d4879a6ee0977a
Raffael Oliveira <80132535+RaffaelOliveiraPuc@users.noreply.github.com>
2022-04-06 12:32:20 -0300

    Initial commit

---
+++ src/Components/ServerList/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <ServerButton isHome />
+    <div class="separator"></div>
+    <ServerButton :mentions="3" />
+    <ServerButton hasNotification />
+    <ServerButton :mentions="45" hasNotification />
+    <ServerButton hasNotification />
+    <ServerButton />
+    <ServerButton hasNotification :mentions="13" />
+    <ServerButton />
+  </div>
+</template>
+
+<script>
+import ServerButton from "./ServerButton";
+
+export default {
+  components: {
+    ServerButton,
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+.container {
+  grid-area: SL;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--tertiary);
+  padding: 11px 0;
+  max-height: 100vh;
+}
+</style>
