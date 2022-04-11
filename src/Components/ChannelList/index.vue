8b867d3bd884ce8b26cfb767f5cd179f9d075829
Raffael Oliveira <raffaeloliveirap@gmail.com>
2022-04-11 16:41:28 -0300

    Create Channel

---
+++ src/Components/ChannelList/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="channel-list">
+    <div class="category">
+      <div>Canais de Text</div>
+      <div class="category-icon">
+        <AddCategoryIcon :size="21" />
+      </div>
+    </div>
+    <ChannelButton />
+    <ChannelButton />
+    <ChannelButton />
+    <ChannelButton />
+    <ChannelButton />
+  </div>
+</template>
+
+<script>
+import AddCategoryIcon from "vue-material-design-icons/Plus";
+import ChannelButton from "./ChannelButton";
+export default {
+  components: {
+    ChannelButton,
+    AddCategoryIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.channel-list {
+  grid-area: CL;
+  display: flex;
+  flex-direction: column;
+  padding: 24px 10px 0 16px;
+  background-color: var(--secondary);
+}
+
+.category {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 6px;
+  color: var(--grey);
+  font-size: 12px;
+  font-weight: 500;
+  text-transform: uppercase;
+  .category-icon {
+    color: var(--symbol);
+    cursor: pointer;
+  }
+}
+</style>
